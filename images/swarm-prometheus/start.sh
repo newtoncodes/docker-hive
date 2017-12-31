af0803fc97d946cc9914624fcc2905110774f275
Christian Nikolov <christian@newton.codes>
2017-12-31 08:02:27 +0200

    /etc/prometheus/prometheus.yml missing fixed

--- images/swarm-prometheus/start.sh
+++ images/swarm-prometheus/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
-
 if [ ${JOBS+x} ]; then
 
 for job in $JOBS
@@ -10,7 +8,7 @@ do
 HOST=$(echo "$job" | cut -d":" -f1)
 PORT=$(echo "$job" | cut -d":" -f2)
 
-cat >>/tmp/prometheus.yml <<EOF
+cat >>/etc/prometheus/prometheus.yml <<EOF
 
   - job_name: '${HOST}'
     dns_sd_configs:
@@ -24,8 +22,6 @@ done
 
 fi
 
-rm -rf /etc/prometheus/prometheus.yml
-mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
 
 /bin/prometheus \
  --config.file="/etc/prometheus/prometheus.yml", \
