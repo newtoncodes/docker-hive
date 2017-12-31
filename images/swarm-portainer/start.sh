f82d295ede73057cf574f8c808142d0a98fdf9ff
Christian Nikolov <christian@newton.codes>
2017-12-31 16:48:39 +0200

    Fix start sh

--- images/swarm-portainer/start.sh
+++ images/swarm-portainer/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-HTPASSWD=`htpasswd -nb -B "$ADMIN_USERNAME" "$ADMIN_PASSWORD" | cut -d ":" -f 2`
+HTPASSWD=`htpasswd -nb -B ${ADMIN_USERNAME} ${ADMIN_PASSWORD} | cut -d ":" -f 2`
+HTPASSWD2=`htpasswd -nb -B admin adminadmin | cut -d ":" -f 2`
+HTPASSWD3=`htpasswd -nb -B admin password | cut -d ":" -f 2`
 
-/portainer -H "unix:///var/run/docker.sock" --admin-password="'$HTPASSWD'"
+echo "$HTPASSWD"
+echo "$HTPASSWD2"
+echo "$HTPASSWD3"
+
+/portainer -H "unix:///var/run/docker.sock" --admin-password="'$HTPASSWD3'"
