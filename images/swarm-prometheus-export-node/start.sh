132dbaf57dfc5b12bd2ed9f12bd1b42431bab7dd
Christian Nikolov <christian@newton.codes>
2017-12-31 05:10:08 +0200

    Reorganize node-exporter

--- images/swarm-prometheus-export-node/start.sh
+++ images/swarm-prometheus-export-node/start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 
-mkdir -p /etc/node-exporter
-
 NODE_NAME=$(cat /etc/nodename)
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
 
-set -- /bin/node_exporter "$@"
-
-exec "$@"
+/bin/node_exporter \
+ --collector.textfile.directory="/etc/node-exporter/" \
+ --collector.procfs="/host/proc" \
+ --collector.sysfs="/host/sys" \
+ --collector.filesystem.ignored-mount-points="^/(sys|proc|dev|host|etc)($$|/)" \
+ --collectors.enabled="textfile,conntrack,diskstats,entropy,filefd,filesystem,loadavg,mdadm,meminfo,netdev,netstat,stat,time,vmstat" \
