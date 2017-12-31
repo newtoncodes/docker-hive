939b2b2ea3d6960ee230b3f5cd5d8d8c21facfb7
Christian Nikolov <christian@newton.codes>
2017-12-31 16:39:21 +0200

    Fix portainer start.sh

--- images/swarm-portainer/start.sh
+++ images/swarm-portainer/start.sh
@@ -2,4 +2,4 @@
 
 HTPASSWD=`htpasswd -nb -B "$ADMIN_USERNAME" "$ADMIN_PASSWORD" | cut -d ":" -f 2`
 
-/portainer -H "unix:///var/run/docker.sock" --admin-password="$ADMIN_USERNAME:$HTPASSWD"
+/portainer -H "unix:///var/run/docker.sock" --admin-password="'$HTPASSWD'"
