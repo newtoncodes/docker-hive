2ae766ef4ccc63b107a6b1beb637d1bd5fc2fd1f
Christian Nikolov <christian@newton.codes>
2017-12-31 18:55:43 +0200

    Portainer env auth

--- images/swarm-portainer/start.sh
+++ images/swarm-portainer/start.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
-HTPASSWD=`htpasswd -nb -B ${ADMIN_USERNAME} ${ADMIN_PASSWORD} | cut -d ":" -f 2`
-HTPASSWD2=`htpasswd -nb -B admin adminadmin | cut -d ":" -f 2`
-HTPASSWD3=`htpasswd -nb -B admin password | cut -d ":" -f 2`
+# HTPASSWD=`htpasswd -nbB ${ADMIN_USERNAME} ${ADMIN_PASSWORD} | cut -d ":" -f 2`
+# /portainer -H "unix:///var/run/docker.sock" --admin-password="'$HTPASSWD'"
 
-echo "$HTPASSWD"
-echo "$HTPASSWD2"
-echo "$HTPASSWD3"
+# Set the user and password via the API, because the CLI doesn't allow choosing an admin username. This way it works like auth.
 
-/portainer -H "unix:///var/run/docker.sock" --admin-password="'$HTPASSWD3'"
+portainer_api() {
+  local verb=$1
+  local url=$2
+  local params=$3
+  local body=$4
+  local response
+  local cmd
+
+  cmd="curl -L -s --fail -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X ${verb} -k http://127.0.0.1:9000${url}"
+  [[ -n "${params}" ]] && cmd="${cmd} -d \"${params}\""
+  [[ -n "${body}" ]] && cmd="${cmd} --data '${body}'"
+  echo "Running ${cmd}"
+  eval ${cmd} || return 1
+  return 0
+}
+
+wait_for_api() {
+  while ! portainer_api GET /
+  do
+    sleep 5
+  done
+}
+
+install_user() {
+  echo "Installing admin user..."
+  if portainer_api POST /api/users/admin/init "" "{\"Username\":\"${ADMIN_USERNAME}\",\"Password\":\"${ADMIN_PASSWORD}\"}"; then
+    echo "installed ok"
+  else
+    echo "install failed"
+  fi
+}
+
+configure_portainer() {
+  wait_for_api
+  install_user
+}
+
+echo "Running configure_portainer in the background..."
+configure_portainer &
+/portainer -H "unix:///var/run/docker.sock" --no-analytics
+exit 0
