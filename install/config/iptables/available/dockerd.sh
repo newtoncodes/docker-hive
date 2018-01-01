ee33be9c7d6a1005a8a080796a288fe911342cf5
Christian Nikolov <christian@newton.codes>
2018-01-01 06:29:36 +0200

    docker iptables added swarm rules

--- install/config/iptables/available/dockerd.sh
+++ install/config/iptables/available/dockerd.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-IP_EXTERNAL="172.18.0.1"
+IP_1=`cut -d'/' -f1 <<< \`ip -o addr show docker0 | awk '{print $4}'\``
+IP_2=`cut -d'/' -f1 <<< \`ip -o addr show docker_gwbridge | awk '{print $4}'\``
 
 
-# SSH to a specific IP
-iptables -A INPUT  -p tcp -d "$IP_EXTERNAL" --dport 9323 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp -s "$IP_EXTERNAL" --sport 9323 -m state --state ESTABLISHED     -j ACCEPT
+# Allow docker ports
+
+# Swarm ports
+if [ "$IP_1" != "" ]; then
+    iptables -A INPUT  -p tcp -d ${IP_1}/24 --dport 2376 -j ACCEPT
+    iptables -A INPUT  -p tcp -d ${IP_1}/24 --dport 2377 -j ACCEPT
+    iptables -A INPUT  -p tcp -d ${IP_1}/24 --dport 7946 -j ACCEPT
+    iptables -A INPUT  -p udp -d ${IP_1}/24 --dport 7946 -j ACCEPT
+    iptables -A INPUT  -p udp -d ${IP_1}/24 --dport 4789 -j ACCEPT
+fi
+if [ "$IP_2" != "" ]; then
+    iptables -A INPUT  -p tcp -d ${IP_2}/24 --dport 2376 -j ACCEPT
+    iptables -A INPUT  -p tcp -d ${IP_2}/24 --dport 2377 -j ACCEPT
+    iptables -A INPUT  -p tcp -d ${IP_2}/24 --dport 7946 -j ACCEPT
+    iptables -A INPUT  -p udp -d ${IP_2}/24 --dport 7946 -j ACCEPT
+    iptables -A INPUT  -p udp -d ${IP_2}/24 --dport 4789 -j ACCEPT
+fi
+
+# Dockerd metrics
+iptables -A INPUT  -p tcp -d "$IP_2" --dport 9323 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -s "$IP_2" --sport 9323 -m state --state ESTABLISHED     -j ACCEPT
